a6a19774f31dea7b1eeefbdb51133e7802fa2ef4
Jorge Muñoz Molina <172324959+joralmumo@users.noreply.github.com>
2025-08-31 21:49:33 -0400

    Tailwind, DaisyUI y Routing
    Listos e instalados tailwind y daisyUI
    
    Listo el routing básico de las páginas.

--- InstacotizaOnline/src/app/app.routes.ts
+++ InstacotizaOnline/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'acerca-de', component: AcercaDeComponent} 
+];
